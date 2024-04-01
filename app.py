d57cc2d9ab12a55ee7de971c7d24e0fe6faf8a6a
fedeleorlando2002 <fedeleorlando056@gmail.com>
2024-04-01 10:54:56 +0200

    funzionamento dello swagger

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/libreria'  # Imposta il nome del database come 'libreria'
 app.config['API_TITLE'] = 'app_name'
 app.config['API_VERSION'] = 'app_version'
-app.config['OPENAPI_VERSION'] = '3.0' 
+app.config['OPENAPI_VERSION'] = '3.0.0' 
 app.config['OPENAPI_URL_PREFIX'] = '/swagger'
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/ui"
-app.config['OPENAPI_URL_UI_URL']= 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui-dist/3.24.2/'
+app.config['OPENAPI_SWAGGER_UI_PATH'] = '/ui'  # Imposta l'endpoint per Swagger UI
+app.config['OPENAPI_SWAGGER_UI_URL'] = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.24.2/'
 
 with app.app_context():
     app.config['DEFAULT_MONGO_INSTANCE'] = PyMongo(app)
@@ -23,7 +23,5 @@ with app.app_context():
     from app_modules.Clienti.views import clienti_blp
     api.register_blueprint(clienti_blp)
 
-    if __name__ == '__main__':
-        app.run(debug=True, port=5001)
-
-
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
