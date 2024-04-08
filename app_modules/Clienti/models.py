41373fb4edb52221f5c21e1d34240948e195551b
fedeleorlando2002 <fedeleorlando056@gmail.com>
2024-04-08 14:42:16 +0200

    modificato il campo telefono

--- app_modules/Clienti/models.py
+++ app_modules/Clienti/models.py
@@ -1,3 +1,4 @@
+from numbers import Number
 from typing import Optional
 from bson import ObjectId
 from dataclasses import field
@@ -7,7 +8,7 @@ class Clienti():
     cognome: str = field(metadata=dict(description="Autore"))
     eta: str = field(metadata=dict(description="Data di nascita")) 
     email: str = field(metadata=dict(description="Email (deve contenere '@')"))
-    telefono: str = field(metadata=dict(description="Telefono"))
+    telefono: str = field(metadata=dict(description="Telefono (massimo 10 numeri)"))
 
     _id: Optional[ObjectId] = field(default=None, metadata=dict(
         dump_only=True, description="ID univoco"))
@@ -28,3 +29,10 @@ class Clienti():
     def __post_init__(self):
         if self.email and '@' not in self.email:
             raise ValueError("L'indirizzo email deve contenere '@'")
+        
+    def __post_init__(self):
+        if self.telefono and not self.is_valid_phone():
+            raise ValueError("Il numero di telefono deve contenere al massimo 10 cifre e non contenere lettere")
+
+    def is_valid_phone(self):
+        return self.telefono.isdigit() and len(self.telefono) <= 10
--- app_modules/Clienti/schemas.py
+++ app_modules/Clienti/schemas.py
@@ -4,10 +4,13 @@ from marshmallow import ValidationError, fields
 
 class ClientiSchema(class_schema(Clienti)):
     email = fields.Str(required=True, metadata=dict(description="Email (deve contenere '@')"))
+    telefono = fields.Str(required=True, metadata=dict(description="Telefono (massimo 10 numeri)"))
 
     def handle_error(self, exc, data, **kwargs):
         if isinstance(exc, ValidationError) and 'email' in exc.messages:
             exc.messages['email'].append("L'indirizzo email deve contenere '@'")
+        if 'telefono' in exc.messages:
+            exc.messages['telefono'].append("Il numero di telefono deve contenere al massimo 10 numeri")
         super().handle_error(exc, data, **kwargs)
 
 # ClientiSchema = class_schema(Clienti)
